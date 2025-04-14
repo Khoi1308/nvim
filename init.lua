35d0eb74d5d32c5d48ef5c84a7e586887cfbd550
Duong Minh Anh Khoi <duongminhanhkhoi14092002@gmail.com>
2025-04-14 19:00:01 +0700

    [ADD]: jdtls plugin & readme file

--- init.lua
+++ init.lua
@@ -1,23 +1,24 @@
 if vim.g.neovide then
-  vim.o.guifont = "DejaVuSansM Nerd Font Mono:h14"
+	vim.o.guifont = "DejaVuSansM Nerd Font Mono:h14"
 end
 
 -- Declare the path where lazy will clone plugin code
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 -- Check if not clone lazy-vim, it clone into lazy.nvim directory
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("configs.options")
 require("configs.keymaps")
+require("configs.autocmds")
 -- Package management
 require("lazy").setup("plugins")
